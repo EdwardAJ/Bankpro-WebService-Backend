717e73231f7cd16fab5e2b08b61cb51b285a81ab
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-11-06 17:00:39 +0700

    Add database-related SQL queries

---
+++ ws-bank/src/main/java/com/webservice/migrate/drop_tables.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS virtual_accounts;
+DROP TABLE IF EXISTS transactions;
---
+++ ws-bank/src/main/java/com/webservice/migrate/seed_tables.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users`
+    (`account_number`, `name`, `created_at`)
+VALUES
+    ('13517115', 'Edward Alexander Jaya', now()),
+    ('13517070', 'Aidil Rezjki Suljztan Syawaludin', now()),
+    ('13516088', 'Tanor Abraham Reyuko', now());
