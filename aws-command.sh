d770013750f673b570b1e3c914712a186c002704
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-11-14 15:33:29 +0700

    Add sleep

--- aws-command.sh
+++ aws-command.sh
@@ -2,8 +2,8 @@ cd /home/ubuntu/ws-bank
 git stash
 git pull origin feature-virtualaccount
 echo 'Removing existing container...'
-sudo docker stop wsbankcontainer
+sudo docker stop wsbankcontainer && sleep 10
 echo 'Building images...'
 sudo docker build -t wsbank .
 echo 'Run the container...'
-sudo docker run -d --rm --name wsbankcontainer -p 8080:8081 wsbank
+sudo docker run -d --rm --name wsbankcontainer -p 8081:8081 wsbank
