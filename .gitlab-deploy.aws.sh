209a000bb8d101ca993c4346577c20526ecce95a
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-11-14 11:38:35 +0700

    Change .sh contents

--- .gitlab-deploy.aws.sh
+++ .gitlab-deploy.aws.sh
@@ -1,13 +1,17 @@
-# !/bin/bash
-# Get servers list:
-set — f
-# Variables from GitLab server:
-# Note: They can’t have spaces!!
-string=$DEPLOY_SERVER
-array=(${string//,/ })
-# Iterate servers for deploy and pull last commit
-# Careful with the ; https://stackoverflow.com/a/20666248/1057052
-for i in “${!array[@]}”; do
-  echo “Deploy project on server ${array[i]}”
-  ssh ubuntu@${array[i]} “cd ~/ws-bank && git stash && git pull origin feature-virtualaccount && sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=wsbank --format="{{.ID}}")) && sudo docker build -t wsbank . && sudo docker run -d --rm -p 8080:8080 wsbank”
+# we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
+# comma seperated values of ip addresses.
+DEPLOY_SERVERS=$DEPLOY_SERVER
+
+# lets split this string and convert this into array
+# In UNIX, we can use this commond to do this
+# ${string//substring/replacement}
+# our substring is "," and we replace it with nothing.
+ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
+echo "ALL_SERVERS ${ALL_SERVERS}"
+
+for server in "${ALL_SERVERS[@]}"
+do
+  echo "deploying to ${server}"
+  ssh ubuntu@${server} “cd ~/ws-bank && git stash && git pull origin feature-virtualaccount && sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=wsbank --format="{{.ID}}")) && sudo docker build -t wsbank . && sudo docker run -d --rm -p 8080:8080 wsbank”
 done
+
