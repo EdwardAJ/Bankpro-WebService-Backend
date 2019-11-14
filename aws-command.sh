a7bdde79edeaf93659992074e657ec32802c8fc2
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-11-14 14:14:50 +0700

    Sudo visudo config

--- aws-command.sh
+++ aws-command.sh
@@ -1,6 +1,6 @@
 cd ws-bank
 git stash
 git pull origin feature-virtualaccount
-echo 'runner' | sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=wsbank --format="{{.ID}}"))
-echo 'runner' | sudo docker build -t wsbank .
-echo 'runner' | sudo docker run -d --rm -p 8080:8080 wsbank”
+sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=wsbank --format="{{.ID}}"))
+sudo docker build -t wsbank .
+sudo docker run -d --rm -p 8080:8080 wsbank”
