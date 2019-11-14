ecf09255017ef753dff3212c3363abacc1b1e243
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-11-14 14:25:53 +0700

    Minor changes in commands

--- aws-command.sh
+++ aws-command.sh
@@ -1,6 +1,9 @@
-cd ws-bank
+cd /home/ubuntu/ws-bank
 git stash
 git pull origin feature-virtualaccount
+echo 'Removing existing container...'
 sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=wsbank --format="{{.ID}}"))
+echo 'Building images...'
 sudo docker build -t wsbank .
-sudo docker run -d --rm -p 8080:8080 wsbank”
+echo 'Run the container...'
+sudo docker run -d --rm -p 8080:8080 wsbank
